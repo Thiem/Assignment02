bd3ffe95914587710451626399bed783784ff8a7
Thiem <tranthiem1993@gmail.com>
2021-06-13 10:48:50 +0700

    Assignment02

---
+++ Assignment02/Exercise01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Assignment02
+{
+    class Exercise01
+    {
+        static void Main(string[] args)
+        {
+            string fullName;
+            bool confirmContinue = true;
+            while (confirmContinue)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your name: ");
+                    fullName = Console.ReadLine();
+                    Console.WriteLine("Enter your year of birth: ");
+                    var year = Console.ReadLine();
+
+                    if (int.Parse(year) > 2021)
+                    {
+                        Console.WriteLine($"Year of birth must <= {DateTime.Now.Year}");
+                    }
+                    else
+                    {
+                        int age = DateTime.Now.Year - int.Parse(year);
+                        Console.WriteLine($"Name: {fullName}");
+                        Console.WriteLine($"Year of Birth: {year}");
+                        Console.WriteLine($"Age: {age}");
+                        Console.WriteLine($"Age in 2010: {2010 - int.Parse(year)}");
+                        Console.WriteLine("Continue? (Y/N)");
+                        string confirm = Console.ReadLine();
+                        if (confirm.ToUpper().Equals("Y"))
+                        {
+                            confirmContinue = true;
+                        }
+                        else
+                        {
+                            confirmContinue = false;
+                            Environment.Exit(0);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
